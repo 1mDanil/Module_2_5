63d8f29283f771790771adee4769696c05bc0f89
1mDanil <danya7540@gmail.com>
2024-11-05 01:13:22 +0300

    Add files via upload

---
+++ Module_2_5.py
@@ -0,0 +1,13 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    print(matrix)
+    return matrix
+n = int(input())
+m = int(input())
+value = int(input())
+matrix = get_matrix(n, m, value)
+
